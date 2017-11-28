d25183c0e3c5fb9afecb28d7cd0db683f8b9e4be
William Awaji <william.hiroshi@gmail.com>
2017-11-27 23:52:52 -0200

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by William Awaji on 27/11/2017.
+//  Copyright © 2017 WA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
